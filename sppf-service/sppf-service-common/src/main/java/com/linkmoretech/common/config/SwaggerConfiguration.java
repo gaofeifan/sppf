f4b6024f14981bcf451c924dd897680a0bdf08be
jiaohanbin <jiaohanbin@papayixing.com>
2019-06-20 14:16:19 +0800

    优化swagger，需在yml文件中配置

--- sppf-service/sppf-service-common/src/main/java/com/linkmoretech/common/config/SwaggerConfiguration.java
+++ sppf-service/sppf-service-common/src/main/java/com/linkmoretech/common/config/SwaggerConfiguration.java
@@ -1,7 +1,16 @@
 package com.linkmoretech.common.config;
 
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import com.google.common.base.Function;
+import com.google.common.base.Optional;
+import com.google.common.base.Predicate;
+
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import springfox.documentation.RequestHandler;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -19,22 +28,71 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
  */
 @Configuration
 @EnableSwagger2
+@ConfigurationProperties(prefix = "swagger")
+@Data
+@Slf4j
 public class SwaggerConfiguration {
-
-
+	
+	private String title = "应用接口";
+	private String description = "本文档 完全基于RESTful接口说明";
+	private String version = "1.0";
+	private String contact = "凌猫停车"; 
+	private String basePackage = "";
+	
     @Bean
     public Docket createRestApi() {
         return new Docket(DocumentationType.SWAGGER_2).apiInfo(apiInfo())
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.linkmoretech.user.controller"))
+                //.apis(basePackage("com.linkmoretech.user.controller,com.linkmoretech.order.controller"))
+                .apis(RequestHandlerSelectors.basePackage(this.basePackage))
                 .paths(PathSelectors.any()).build();
     }
     @Bean
     public ApiInfo apiInfo() {
-        return new ApiInfoBuilder().title("用户服务")
-                .description("凌猫停车大后台服务")
-                .contact(new Contact("Alec","al","qlzw226@sina.com"))
-                .version("1.0")
+        return new ApiInfoBuilder().title(this.title)
+                .description(this.description)
+                .contact(new Contact(this.contact,null,null))
+                .version(this.version)
                 .build();
+    }
+    
+    public static Predicate<RequestHandler> basePackage(final String basePackage) {
+		log.info("basePackage ======================= {} " + basePackage);
+        return new Predicate<RequestHandler>() {
+            @Override
+            public boolean apply(RequestHandler input) {
+                return declaringClass(input).transform(handlerPackage(basePackage)).or(true);
+            }
+        };
+    }
+    
+    /**
+     * 处理包路径配置规则,支持多路径扫描匹配以逗号隔开
+     * 
+     * @param basePackage 扫描包路径
+     * @return Function
+     */
+    private static Function<Class<?>, Boolean> handlerPackage(final String basePackage) {
+        return new Function<Class<?>, Boolean>() {
+            
+            @Override
+            public Boolean apply(Class<?> input) {
+                for (String strPackage : basePackage.split(",")) {
+                    boolean isMatch = input.getPackage().getName().startsWith(strPackage);
+                    if (isMatch) {
+                        return true;
+                    }
+                }
+                return false;
+            }
+        };
+    }
+    
+    /**
+     * @param input RequestHandler
+     * @return Optional
+     */
+    private static Optional<? extends Class<?>> declaringClass(RequestHandler input) {
+        return Optional.fromNullable(input.getClass());
     }
 }
