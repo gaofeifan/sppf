7a1dcd2757ab2ea1bf28a3a8b234d1e34a3f7fb8
alec <qlzw226@sina.com>
2019-07-03 18:14:40 +0800

    优化

---
+++ sppf-service/sppf-service-common/src/main/java/com/linkmoretech/common/config/FeignOkHttpConfig.java
@@ -0,0 +1,34 @@
+package com.linkmoretech.common.config;
+
+import feign.Feign;
+import okhttp3.ConnectionPool;
+import org.springframework.boot.autoconfigure.AutoConfigureBefore;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.cloud.openfeign.FeignAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @Author: alec
+ * Description: 用OKHttp3 替代 httpClient
+ * @date: 17:27 2019-07-03
+ */
+@Configuration
+@ConditionalOnClass(Feign.class)
+@AutoConfigureBefore(FeignAutoConfiguration.class)
+public class FeignOkHttpConfig {
+
+    @Bean
+    public okhttp3.OkHttpClient okHttpClient(){
+        return new okhttp3.OkHttpClient.Builder()
+                .readTimeout(60, TimeUnit.SECONDS)
+                .connectTimeout(60, TimeUnit.SECONDS)
+                .writeTimeout(120, TimeUnit.SECONDS)
+                .connectionPool(new ConnectionPool())
+                // .addInterceptor();
+                .build();
+    }
+
+}
